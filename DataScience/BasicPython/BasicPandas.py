4293f26cad2f99ca4feeedfc699c7b12cc570d84
jitu <amkhanshadhin@gmail.com>
2023-04-15 14:26:21 +0600

    Started Basic Pandas

--- DataScience/BasicPython/BasicPandas.py
+++ DataScience/BasicPython/BasicPandas.py
@@ -79,3 +79,40 @@ print(MarketData.head())
 #prints first 2 rows of the dataset
 print("first two rows of the dataset")
 print(MarketData.head(2))
+
+# ###Read From CSV File
+# import numpy  as nmpy
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# print("Here")
+
+# dataset= pd.read_csv('Drawable/DataSet.csv')
+# X=dataset.iloc[:,1:2].values
+# print(X)
+
+# dataset.plot()
+# plt.show()
+
+# s=pd.Series([1,2,3,"f"])
+# date=pd.date_range('20200829',periods=6)
+# print(date)
+
+# df=pd.DataFrame(nmpy.random.randn(6,4),index=date,columns=list('ABCD'))
+# print(df)
+
+# df['E']=['One','Two','Three','Four','Five','Six']
+# print(df)
+
+# s1=pd.Series([1,2,3,4,5,6],index=pd.date_range('20200904', periods=6))
+# print(s1)
+
+# df['F']=s1
+# print(df)
+
+# df.to_excel('dateSet.xlsx',sheet_name='sheet1')
+# df1=pd.read_excel('dateSet.xlsx',sheet_name='sheet1',index_col=None,na_values=['Na'])
+# print(df1)
+
+# #df=df.cumsum()
+# df.plot()
+# plt.show()

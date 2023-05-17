e09331292499892bf7f1cd2bc6ed85702cea3479
jitu <amkhanshadhin@gmail.com>
2023-05-18 01:28:33 +0600

    djanto thik korsi

--- LearningDjangoRESTAPI/mydjango/mydjango/urls.py
+++ LearningDjangoRESTAPI/mydjango/mydjango/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-#from django.urls import include, re_path as path
+#from django.urls import path, include
+from django.urls import include, re_path as path
 #from . import views
 from rest_framework.authtoken.views import obtain_auth_token
 from firstApp.api.views import homeView
